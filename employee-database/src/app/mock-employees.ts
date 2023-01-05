06d9516e6843c708d7e1dbb51dc954e2477a71f3
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2023-01-05 22:57:46 +0100

    add mock-employees before update

---
+++ employee-database/src/app/mock-employees.ts
@@ -0,0 +1,44 @@
+import { Employee } from './employee';
+
+export const EMPLOYEES: Employee[] = [
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+  {
+    id: 1234,
+    name: 'Blah Blabla',
+    email: 'blah@itera.no',
+  },
+];
