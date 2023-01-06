67c5e28083a5401fe0f71367f52bf57a0958a874
Ola Fredrik Ansnes Kveli <olakveli@gmail.com>
2023-01-06 08:02:30 +0100

    update to add employees properly to list

--- employee-database/src/app/employees/employees.component.ts
+++ employee-database/src/app/employees/employees.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EMPLOYEES } from '../mock-employees';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+  styleUrls: ['./employees.component.css'],
 })
+export class EmployeesComponent {
+  employees = EMPLOYEES;
+  selectedEmployee?: Employee;
+  onSelect(employee: Employee): void {
+    this.selectedEmployee = employee;
+  }
+}
+
+/*
 export class EmployeesComponent implements OnInit {
-  
-  newEmployee = "Ingunn Tangen Era"
+  newEmployee = 'Ingunn Tangen Era';
 
   employee: Employee = {
-    id:1,
-    name:"Arne Mjøs",
-    email: "test@test.no"
-  }
-  constructor() { 
-  }
+    id: 1,
+    name: 'Arne Mjøs',
+    email: 'test@test.no',
+  };
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
+*/
